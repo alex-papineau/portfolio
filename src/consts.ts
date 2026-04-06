bb40366b799608e715af15f3e51288c1ea383711
@ <99501899+alex-papineau@users.noreply.github.com>
2026-04-05 23:52:03 -0500

    ai changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Alex Papineau";
+export const SITE_DESCRIPTION = "The personal portfolio of Alex Papineau. Enter the terminal.";
