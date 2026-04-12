ff9f89ee65289b9ea422508c492899ca18f97603
@ <99501899+alex-papineau@users.noreply.github.com>
2026-04-11 23:39:02 -0500

    gorg

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Alex Papineau";
-export const SITE_DESCRIPTION = "The personal portfolio of Alex Papineau. Enter the terminal.";
+export const SITE_TITLE = "gorg";
+export const SITE_DESCRIPTION = "gorg";
